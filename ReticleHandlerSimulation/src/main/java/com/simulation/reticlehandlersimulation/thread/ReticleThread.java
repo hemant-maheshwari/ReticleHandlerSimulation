65ed06e417ccb27a43fcb02576c9010a8e231e2c
Hemant Maheshwari <hemantm@Hemants-MacBook-Pro.local>
2020-11-10 11:12:59 -0500

    Runnable JAR

--- ReticleHandlerSimulation/src/main/java/com/simulation/reticlehandlersimulation/thread/ReticleThread.java
+++ ReticleHandlerSimulation/src/main/java/com/simulation/reticlehandlersimulation/thread/ReticleThread.java
@@ -17,13 +17,18 @@ public class ReticleThread implements Runnable{
     @Override
     public void run() {
         System.out.println("Running Thread for Reticle:"+ reticleName);
+        System.out.println();
         try{
             for(Path path: paths){
                 System.out.println("Moving reticle "+reticleName+" from station: "+ path.getFromStation());
                 Thread.sleep(path.getTravelTime());
                 System.out.println("Moved reticle "+reticleName+" to station: "+path.getToStation());
+                System.out.println();
+                System.out.println("Reticle took "+path.getTravelTime()/1000+" seconds to move from "+path.getFromStation()+" to "+path.getToStation());
+                System.out.println();
             }
             System.out.println("Reticle "+reticleName+" exited.");
+            System.out.println("*******************************************");
         }catch(InterruptedException e){
             System.out.println("Reticle "+reticleName+" interrupted.");
         }
